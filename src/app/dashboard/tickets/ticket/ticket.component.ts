dab9e164496a0499e62737fbe5862c7d22cf40e0
Ujjwal Kumar Agrawal <ujjwalagrawal29102001@gmail.com>
2025-02-16 15:34:31 +0530

    Added collapsable logic

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { TicketModel } from './ticket.model';
 
 @Component({
@@ -10,4 +10,10 @@ import { TicketModel } from './ticket.model';
 })
 export class TicketComponent {
   data = input.required<TicketModel>();
+  detailsVisible = signal(false);
+
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
