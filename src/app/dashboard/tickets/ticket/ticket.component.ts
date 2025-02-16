cf6eea437f28fa65cdd676cbc66f1bf38e68c65d
Ujjwal Kumar Agrawal <ujjwalagrawal29102001@gmail.com>
2025-02-16 15:42:26 +0530

    Added logic to 'Mark as completed' Button

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { TicketModel } from './ticket.model';
 
 @Component({
@@ -10,10 +10,15 @@ import { TicketModel } from './ticket.model';
 })
 export class TicketComponent {
   data = input.required<TicketModel>();
+  close = output();
   detailsVisible = signal(false);
 
   onToggleDetails() {
     // this.detailsVisible.set(!this.detailsVisible());
     this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
   }
 }
